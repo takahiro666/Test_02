ff63babc6fdb11e2454922741a3815a2c7966514
aoitomo1031 <nkcgta.yutaro.aoki@gmail.com>
2020-07-21 14:12:45 +0900

    Fadeとかいろいろ作業中

---
+++ HGS_Chess/Assets/Script/Button.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Button : MonoBehaviour
+{
+    public Fade fade;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void StartButton()
+    {
+        if (Input.GetButtonDown("StartScene"))
+        {
+            fade.FadeIn(0.5f, () => print("フェードイン完了"));
+            SceneManager.LoadScene("GameScene");
+            fade.FadeOut(0.5f, () => print("フェードアウト完了"));
+        }
+
+
+    }
+
+    public void MenuButton()
+    {
+    }
+
+    public void ResultButton()
+    {
+
+    }
+}
