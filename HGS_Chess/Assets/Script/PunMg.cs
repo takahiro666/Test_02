0f234c78cbefbbc1ea113636d4ad91861931f3cb
aoitomo1031 <nkcgta.yutaro.aoki@gmail.com>
2020-09-17 13:58:21 +0900

    Revert "オンライン追加"
    This reverts commit a08193d0cc32f28e1394d36031bf997c6c003b65.

--- HGS_Chess/Assets/Script/PunMg.cs
+++ HGS_Chess/Assets/Script/PunMg.cs
@@ -9,7 +9,6 @@ public class PunMg : MonoBehaviourPunCallbacks
     {
         // Photonに接続する(引数でゲームのバージョンを指定できる)
         PhotonNetwork.ConnectUsingSettings();
-        Debug.Log("接続");
     }
     void Update()
     {
@@ -20,14 +19,13 @@ public class PunMg : MonoBehaviourPunCallbacks
     {
         //"room"という名前のルームに参加する(なかったら作って参加)
         PhotonNetwork.JoinOrCreateRoom("room", new RoomOptions(), TypedLobby.Default);
-        Debug.Log("参加");
     }
+
     // マッチングが成功した時に呼ばれるコールバック
     public override void OnJoinedRoom()
     {
         // マッチング後、ランダムな位置に自分自身のネットワークオブジェクトを生成する
         var v = new Vector3(Random.Range(-3f, 3f), Random.Range(-3f, 3f));
-        PhotonNetwork.Instantiate("Cube", v, Quaternion.identity,0);
-        Debug.Log("生成");
+        PhotonNetwork.Instantiate("GamePlayer", v, Quaternion.identity);
     }
 }
