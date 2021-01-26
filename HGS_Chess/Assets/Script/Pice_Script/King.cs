69318d91ebcb3299fed6520ffbbaf70ac8138953
hyuga <kurihara.73091@gmail.com>
2021-01-26 15:06:34 +0900

    オンライン削除してUIなどの調整

--- HGS_Chess/Assets/Script/Pice_Script/King.cs
+++ HGS_Chess/Assets/Script/Pice_Script/King.cs
@@ -1,23 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Photon.Pun;
-using Photon.Realtime;
 
 public class King : Move
 {
     public int hp = 3;
     public int at = 1;
-    OnlinePice Pos;
-    private PhotonView photonView = null;
+    Pice Pos;
 
-     void Awake()
-    {
-        photonView = GetComponent<PhotonView>(); 
-    }
     public override bool[,] PossibleMove()
     {
-        Pos = GameObject.Find("gamelot").GetComponent<OnlinePice>();
+        Pos = GameObject.Find("gamelot").GetComponent<Pice>();
        // Pos = GameObject.Find("gamelot").GetComponent<Pice>();
 
         bool[,] r = new bool[Pos.X, Pos.Y];
@@ -29,16 +22,14 @@ public class King : Move
         j = CurrentY + 1;
         if(CurrentY != 6)
         {
-            c = OnlinePice.Instance.moves[CurrentX, CurrentY + 1];
-            //c = Pice.Instance.moves[CurrentX, CurrentY + 1];
+            c = Pice.Instance.moves[CurrentX, CurrentY + 1];
             if (c == null)
                 r[CurrentX , CurrentY + 1] = true;
             else if (isWhite != c.isWhite)
                 r[CurrentX , CurrentY + 1] = true;
             if (CurrentX!=0)//左上表示
             {
-                c = OnlinePice.Instance.moves[CurrentX-1, CurrentY + 1];
-                //c = Pice.Instance.moves[CurrentX-1, CurrentY + 1];
+                c = Pice.Instance.moves[CurrentX-1, CurrentY + 1];
                 if (c == null)
                     r[CurrentX-1, CurrentY + 1] = true;
                 else if (isWhite != c.isWhite)
@@ -46,8 +37,7 @@ public class King : Move
             }
             if(CurrentX!=Pos.X-1)
             {
-                c = OnlinePice.Instance.moves[CurrentX+1, CurrentY + 1];
-                //c = Pice.Instance.moves[CurrentX+1, CurrentY + 1];
+                c = Pice.Instance.moves[CurrentX+1, CurrentY + 1];
                 if (c == null)
                     r[CurrentX+1, CurrentY + 1] = true;
                 else if (isWhite != c.isWhite)
@@ -63,16 +53,14 @@ public class King : Move
         j = CurrentY - 1;
         if (CurrentY != 0)
         {
-            c = OnlinePice.Instance.moves[CurrentX, CurrentY - 1];
-            //c = Pice.Instance.moves[CurrentX, CurrentY - 1];
+            c = Pice.Instance.moves[CurrentX, CurrentY - 1];
             if (c == null)
                 r[CurrentX, CurrentY - 1] = true;
             else if (isWhite != c.isWhite)
                 r[CurrentX, CurrentY - 1] = true;
             if (CurrentX != 0)//左上表示
             {
-                c = OnlinePice.Instance.moves[CurrentX - 1, CurrentY - 1];
-               // c = Pice.Instance.moves[CurrentX - 1, CurrentY - 1];
+                c = Pice.Instance.moves[CurrentX - 1, CurrentY - 1];
                 if (c == null)
                     r[CurrentX - 1, CurrentY - 1] = true;
                 else if (isWhite != c.isWhite)
@@ -80,8 +68,7 @@ public class King : Move
             }
             if (CurrentX != Pos.X - 1)
             {
-                c = OnlinePice.Instance.moves[CurrentX + 1, CurrentY - 1];
-                //c = Pice.Instance.moves[CurrentX + 1, CurrentY - 1];
+                c = Pice.Instance.moves[CurrentX + 1, CurrentY - 1];
                 if (c == null)
                     r[CurrentX + 1, CurrentY - 1] = true;
                 else if (isWhite != c.isWhite)
@@ -92,8 +79,7 @@ public class King : Move
         //左(真ん中)
         if(CurrentX != 0)
         {
-            c = OnlinePice.Instance.moves[CurrentX -1, CurrentY];
-            //c = Pice.Instance.moves[CurrentX -1, CurrentY];
+            c = Pice.Instance.moves[CurrentX -1, CurrentY];
             if (c == null)
                 r[CurrentX - 1, CurrentY] = true;
             else if (isWhite != c.isWhite)
@@ -103,8 +89,7 @@ public class King : Move
         //右(真ん中)
         if (CurrentX != 6)
         {
-            c = OnlinePice.Instance.moves[CurrentX +1, CurrentY];
-            //c = Pice.Instance.moves[CurrentX +1, CurrentY];
+            c = Pice.Instance.moves[CurrentX +1, CurrentY];
             if (c == null)
                 r[CurrentX + 1, CurrentY] = true;
             else if (isWhite != c.isWhite)
@@ -112,9 +97,5 @@ public class King : Move
         }
 
         return r;
-    }
-    void Update()
-    {
-       // if (!photonView.IsMine) return; 
     }
 }
